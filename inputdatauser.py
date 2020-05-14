e67771af20ac3af49ea4fb349dbffdd52a2cd00e
said <mail@saidramadhan.com>
2020-05-15 01:19:20 +0700

    4 commit

--- inputdatauser.py
+++ inputdatauser.py
@@ -1,6 +1,16 @@
 #input data user
 
 #data input pasti string
+data1 = input("enter your name:")
+print("Hello "+ data1 + "!" , type(data1))
+data2 = input("your origin: ")
+print("im from " + data2)
+data3 = input("what you like: ")
+print("i love " + data3)
+#jika ingin int, kita casting data lagi :)
+#input data user
+
+#data input pasti string
 data = input("enter your name:")
 print("Hello ", data, "!")
 
