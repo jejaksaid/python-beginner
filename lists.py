dce32dc5b2f8b75408fe14077c7856e1bd2cfe11
said <mail@saidramadhan.com>
2020-05-15 02:14:56 +0700

    lists

--- lists.py
+++ lists.py
@@ -1,5 +1,28 @@
+print('=====================list biasa, index, ')
 hero = ["clinkz", "invoker", "wraith king", "spectre", "bounty hunter"]
 #index dari  0          1            2
 hero[1] = "axe"
 hero[3] = "mirana"
 print(hero [1:4])
+
+#list function
+print('=====================list function, .count,.sort,.pop,'
+      '.index,.extend,.insert,. remove,.append,.clear,. reverse')
+number = [6,4,22,12,2,1,3]
+hero = ["zeus", "doom", "ogre", "ogre", "abaddon", "techies"]
+# hero.extend(number) nambah list dengan number
+# hero.append("tide hunter") nambah list
+# hero.insert(4, "ck") nambah list (letak spesifik)
+# hero.clear() menghapus list
+# hero.pop() menghapus ujung list
+# hero.sort() mengurtkan sesuai alfabet
+# number.sort()mengurutkan sesuai nomer 1- ..
+# number.reverse() mengurutkan dari belakang
+# hero2 = hero.copy() menyalin list
+
+print()
+# print(hero2)
+# print(number)
+# print(number)
+# print(hero.count("ogre")) menghitung jumlah
+# print(hero.index("doom")) menghitung index
