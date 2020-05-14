e7e64b9d9899f12e3245724c00e6a61a87648ccc
said <mail@saidramadhan.com>
2020-05-15 03:35:15 +0700

    function

--- funcions.py
+++ funcions.py
@@ -1,7 +1,17 @@
 print('============function')
-def hi_there(name, age):
-    print("Hello " + name + "you are " + age)
+#didalam function bisa ada string, boolean, float, integer
+# #JANGAN LUPA :
+# 1. penamaan nama():
+# 2. pemanggilan fungsi dibawah
+
+
+
+
+
+def hi_there(name, age, work, heigh):
+    print("Hello " + name + "you are " + str(age)
+          + "and you " + str(bool(work)) + "you are " + str(float(heigh)))
 # print("top")
-hi_there("said", "22")
-hi_there("ganteng" , "24")
+hi_there("said", 22, True, 170 )
+hi_there("ganteng", 24, False, 180 )
 # print("bottom")
