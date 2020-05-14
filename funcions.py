c5dfd33d6ee99d7447bffe4802c4f52060463904
said <mail@saidramadhan.com>
2020-05-15 03:09:56 +0700

    function

---
+++ funcions.py
@@ -0,0 +1,7 @@
+print('============function')
+def hi_there(name, age):
+    print("Hello " + name + "you are " + age)
+# print("top")
+hi_there("said", "22")
+hi_there("ganteng" , "24")
+# print("bottom")
