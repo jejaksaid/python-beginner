3791962363d07d0f6d655a68ee33b8d65961130c
said <mail@saidramadhan.com>
2020-05-15 02:54:29 +0700

    tuples

--- tuples.py
+++ tuples.py
@@ -1,1 +1,10 @@
-print("halo dunia")
+print("==============tuples")
+# 1. berbeda dengan list
+# 2. tidak bisa dirubah , tapi bisa ditambah
+# hanya digunakan untuk situasi tertentu
+coordinates1 = (2, 3) , (4, 5)
+print(coordinates1[1])
+
+# tup=ples didalam list
+coordinates2 = [(2, 3, 4, 5, 6), (1,2,3,4,5)]
+print(coordinates2)
