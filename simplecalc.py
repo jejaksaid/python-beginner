13bd99ffc851d0424c35d6055932bfb7ac4cd6c6
said <mail@saidramadhan.com>
2020-05-18 01:35:48 +0700

    simplecalc

---
+++ simplecalc.py
@@ -0,0 +1,22 @@
+# CALCULATOR SEDERHANA
+
+num1 = float(input("Enter first number :"))
+op = input("Enter Operator :")
+num2 =float(input("Enter second number :"))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "**":
+    print(num1 ** num2)
+elif op == "//":
+    print(num1 // num2)
+elif op == "%":
+    print(num1 % num2)
+else:
+    print("wrong command")
