c2eac3ffae2392c2a2ed32201c02b4b089ee6115
said <mail@saidramadhan.com>
2020-05-19 17:45:21 +0700

    function

--- funcions.py
+++ funcions.py
@@ -5,12 +5,10 @@ print('============function')
 # 2. pemanggilan fungsi dibawah
 
 
-
-
-
 def hi_there(name, age, work, heigh):
-    print("Hello " + name + "you are " + str(age)
-          + "and you " + str(bool(work)) + "you are " + str(float(heigh)))
+    print("Hello " + name + " you are " + str(age)
+          + " and you " + str(bool(work)) + " you are " + str(float(heigh)))
+
 # print("top")
 hi_there("said", 22, True, 170 )
 hi_there("ganteng", 24, False, 180 )
